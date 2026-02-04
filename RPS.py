1b25fcb736f2057a235c26f652a1271e23582d26
NathanHeavican <nheavican@unomaha.edu>
2026-02-04 18:42:14 +0000

    Partially done with lab 3

--- RPS.py
+++ RPS.py
@@ -1,7 +1,8 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: Nathan Heavican
+#Date: 2-4-26
+#Assignment: Lab 3
+#Purpose: Create a playable Rock, Paper, Scissors game where the user selects Rock, Paper, or Scissors, the computer randomly selects a choice, the program determines the outcome, the user can choose to play again, and the program tracks wins, losses, and ties.
 import random
 
 def main():
@@ -12,8 +13,23 @@ def main():
   #User can play as many games as they wish.
 
   #Randomly choose the computer between 'R', 'P', or 'S'
+  computer = random.choice(["R", "P", "S"])
   #Prompt the user for their RPS selection
+  player = input("Choose your weapon (R, P, S): ")
   #Determine winner and state what happened to the user
+  if computer == "R":
+    print("The computer chose Rock.")
+  elif computer == "P":
+    print("The computer chose Paper.")
+  else computer == "S":
+    print("The computer chose Scissors.")
+
+  if player == "R":
+    print("You chose Rock.")
+  elif player == "P":
+    print("You chose Paper.")
+  else player == "S":
+    print("You chose Scissors.")
   #Ask the user if they would like to play again.
 
   #In the end, print the stats
--- TempConvert.py
+++ TempConvert.py
@@ -1,17 +1,16 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: Nathan Heavican
+#Date: 2-4-26
+#Assignment: Lab 3
+#Purpose: Create a program that asks the user for a temperature in Fahrenheit, converts it to Celsius, and displays the result clearly.
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
+  tempF = float(input("Please enter a temperature in Fahrenheit: "))
   #Convert that temperature to celsius, rounding to 1 decimal percision
+  tempC = round((tempF-32)*(5/9), 1)
   #Output converted temperature.
-  tempF = 80
-
-  tempC = tempF / 2
-
-  print(tempF, "is ", tempC, "degrees celsius.")
+  print(tempF, "degrees Fahrenheit is ", tempC, "degrees Celsius.")
 if __name__ == '__main__':
   main()
