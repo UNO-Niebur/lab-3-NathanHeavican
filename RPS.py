d763ed0c9782820e292edca37a250d6edc1e1a37
NathanHeavican <nheavican@unomaha.edu>
2026-02-05 05:22:43 +0000

    finished Lab 3

--- RPS.py
+++ RPS.py
@@ -13,26 +13,37 @@ def main():
   play = "yes"
   while play == "yes":
     #User can play as many games as they wish.
+    #Randomly choose the computer between 'R', 'P', or 'S'
+    computer = random.choice(["R", "P", "S"])
+    #Prompt the user for their RPS selection
+    player = input("Choose your weapon (R, P, S): ")
+    #Determine winner and state what happened to the user
+    if computer == "R":
+      print("The computer chose Rock.")
+    elif computer == "P":
+      print("The computer chose Paper.")
+    elif computer == "S":
+      print("The computer chose Scissors.")
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  computer = random.choice(["R", "P", "S"])
-  #Prompt the user for their RPS selection
-  player = input("Choose your weapon (R, P, S): ")
-  #Determine winner and state what happened to the user
-  if computer == "R":
-    print("The computer chose Rock.")
-  elif computer == "P":
-    print("The computer chose Paper.")
-  else computer == "S":
-    print("The computer chose Scissors.")
+    if player == "R":
+      print("You chose Rock.")
+    elif player == "P":
+      print("You chose Paper.")
+    elif player == "S":
+      print("You chose Scissors.")
 
-  if player == "R":
-    print("You chose Rock.")
-  elif player == "P":
-    print("You chose Paper.")
-  else player == "S":
-    print("You chose Scissors.")
-  #Ask the user if they would like to play again.
+    if player == computer:
+      ties += 1
+      print("You tied.")
+    elif (player == "R" and computer == "S") or (player == "P" and computer == "R") or (player == "S" and computer == "P"):
+      wins += 1
+      print("You win!")
+    else:
+      losses += 1
+      print("You lose!")
+
+    #Ask the user if they would like to play again.
+    play = input("Would you like to play again? (yes or no): ")
 
   #In the end, print the stats
   print("Wins \t Ties \t Losses")
